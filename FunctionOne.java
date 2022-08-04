888e3741cf8c9ba3395f48cb035b594add2ee6ca
siddharth007-singh <siddharth.code.cws@gmail.com>
2022-08-03 22:19:53 -0700

    Thrid commit

---
+++ FunctionOne.java
@@ -0,0 +1,50 @@
+public class FunctionOne{
+
+    // public static void printMyName(String name){
+    //     System.out.println(name);
+    //     return;
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     try{
+    //         String name = sc.next();
+    //         printMyName(name);    
+    //     }finally{
+    //         sc.close();
+    //     }
+    // }
+
+
+    // public static int printMulti(int a, int b){
+    //     int sum;
+    //     sum = a*b;
+    //     return sum;
+    // }
+
+    // public static void main(String[] args) {
+    //     int a = 5;
+    //     int b = 10;
+
+    //     int sum = printMulti(a, b);
+    //     System.out.println("The Product of Two Number : "+ sum);
+    // }    
+
+    // public static void factorialNum(int num){
+    //     int i;
+    //     int fact = 1;
+    //     for(i=num; i>=1; i--){
+    //         fact = fact*i;
+    //     }
+    //     System.out.println(fact);
+    //     return; 
+    // }
+
+    // public static void main(String[] args) {
+    //     int num = 4;
+    //     factorialNum(num);
+    // }
+
+    public static void main(String[] args) {    
+        
+    }
+}
